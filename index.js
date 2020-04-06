bb1cc5e9851a534ec049bfa8b79cafc0c1000828
catalina p <cpimentel.kranio@bicevida.cl>
2020-04-06 00:59:04 -0400

    simple rest operations in dishes

--- index.js
+++ index.js
@@ -1,27 +1,75 @@
-const express = require('express')
-const http = require('http')
+const express = require("express");
+const http = require("http");
 
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
-const hostname = 'localhost'
-const port = '3000'
+const hostname = "localhost";
+const port = "3000";
 const app = express();
 // middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 // looks for local files in that directory
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + "/public"));
+
+// rest api endpoint
+app.all("/dishes", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-type", "text/plain");
+  next();
+});
+
+app.get("/dishes", (req, res, next) => {
+  res.end("will send all the dishes to you!!");
+});
+
+app.post("/dishes", (req, res, next) => {
+  res.end(
+    "will add the dish: " + req.body.name +
+      " with details: " + req.body.description
+  );
+});
+
+app.put("/dishes", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("PUT operation not supported on /dishes");
+});
+
+// danger zone
+app.delete("/dishes", (req, res, next) => {
+  res.end("Deleting all the dishes in /dishes!!");
+});
+
+// dishId
+app.get("/dishes/:dishId", (req, res, next) => {
+  res.end("will send details of the specified dish: " + req.params.dishId) + ' to you. ';
+});
+
+app.post("/dishes/:dishId", (req, res, next) => {
+  res.end("POST operation is not supported on /dishes/" + req.params.dishId);
+});
+
+app.put("/dishes/:dishId", (req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('will update the specified dish: ' + req.body.name +
+  'with these details: ' + req.body.description);
+});
+
+// danger zone
+app.delete("/dishes/:dishId", (req, res, next) => {
+  res.end("Deleting dish: " + req.params.dishId);
+});
 
 app.use((req, res, next) => {
   // console.log(req.headers);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<html><body><h1>express server running!!!</h1></body></html>')
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>express server running!!!</h1></body></html>");
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
   console.log(`server running at http://${hostname}:${port}`);
-})
+});
