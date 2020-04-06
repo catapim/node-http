25d5e04b6822195df323a5068fd294393279e916
catalina p <cpimentel.kranio@bicevida.cl>
2020-04-06 00:18:18 -0400

    body parser added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const http = require('http')
+
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
 const hostname = 'localhost'
 const port = '3000'
 const app = express();
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+// looks for local files in that directory
+app.use(express.static(__dirname+'/public'));
 
 app.use((req, res, next) => {
-  console.log(req.headers);
+  // console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html')
   res.end('<html><body><h1>express server running!!!</h1></body></html>')
